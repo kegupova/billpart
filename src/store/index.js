887573e85cc1fc18d64570730768cbb0f20d0e77
kegupova <63420335+kegupova@users.noreply.github.com>
2023-08-22 09:37:01 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    people: [],
+    items: [],
+    totalSum: 0,
+
   },
+
   getters: {
+    getPeople(state) {
+      return state.people;
+    },
+    getPeopleLength(state) {
+      return state.people.length;
+    },
+    getItemsByPerson: (state) => (person) => {
+      return state.items.filter((item) => item.people.includes(person.name));
+    },
+    getItems(state) {
+      return state.items;
+    },
+
+
   },
+
   mutations: {
+    addPerson(state, person) {
+      state.people.push(person);
+    },
+    removePerson(state, person) {
+      const index = state.people.indexOf(person);
+      if (index !== -1) {
+        state.people.splice(index, 1);
+      }
+    },
+    addItem(state, item) {
+      state.items.push(item);
+      state.totalSum += item.cost;
+    },
+
+
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
